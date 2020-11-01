d1aa17fa929018eac23fc6f73022c92110887827
shmato4ek <58270694+shmato4ek@users.noreply.github.com>
2020-11-01 21:59:22 +0200

    1

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ document.getElementById('b1').onclick = task1
 document.getElementById('b2').onclick = task2
 document.getElementById('b3').onclick = task3
 document.getElementById('b4').onclick = task4
+document.getElementById('b5').onfocus = task5_1
+document.getElementById('b5').onblur  = task5_2
+
+window.onload = () => {
+    paint()
+    setTimeout(() =>{
+        if(getCookie('min')&&getCookie('max')){
+            const min = getCookie('min')
+            const max = getCookie('max')
+            const cookies = confirm('Max: ' + max + ' Min: ' + min + '\nDelete cookies?')
+            if(cookies){
+                removeCookie('min')
+                removeCookie('max')
+                document.querySelector('#min_max').style.visibility = 'visible'
+                location.reload()
+            }
+            else{
+                alert('Page contains cookies, please update it')
+            }
+        }
+        else{
+            document.querySelector('#min_max').style.visibility = 'visible'
+        }
+    }
+    , 1)
+}
 
 function task1(){
     const text2 = document.querySelector('.tag1')
@@ -19,17 +45,41 @@ function task2(){
 }
 
 function task4(){
-    const color = document.querySelector('#border_color').querySelector('input').value
+    const color = document.getElementById('border_color').querySelector('input').value
     localStorage.setItem('border_color',color)
     paint()
 }	
 
 function paint(){
     const color = localStorage.getItem('border_color')
-    document.querySelector('.t1').style.border = ['solid',0.3+'rem',color].join(' ')
+    document.getElementById('text1').style.border = ['solid',0.3+'rem',color].join(' ')
     document.querySelector('.t2').style.border = ['solid',0.3+'rem',color].join(' ')
     document.querySelector('.t3').style.border = ['solid',0.3+'rem',color].join(' ')
     document.querySelector('.t4').style.border = ['solid',0.3+'rem',color].join(' ')
     document.querySelector('.t5').style.border = ['solid',0.3+'rem',color].join(' ')
     document.querySelector('.t6').style.border = ['solid',0.3+'rem',color].join(' ')
+}
+
+function task5_1(){
+    document.getElementById('b5').value = 'Focus'
+}
+function task5_2(){
+    document.getElementById('b5').value = 'Task 5'
+}
+
+function getCookie(name)
+{
+    let cookies = document.cookie.split(';')
+    for(let i = 0;i < cookies.length;i++)
+        if(cookies[i].trim().split('=')[0] == name)
+            return cookies[i].trim().split('=')[1]
+    return null;
+}
+function setCookie(name, value)
+{
+    document.cookie = name + '=' + value;
+}
+function removeCookie (name)
+{
+    document.cookie = name+'=; max-age=-1'
 }
